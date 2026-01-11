d2b6b3216ce7e049815685efbfb8b3c5572a8e33
bonaventuresystems <66865310+bonaventuresystems@users.noreply.github.com>
2026-01-11 23:57:46 +0530

    CheckIN
    CheckIN

---
+++ JDOPSTest/Controllers/StockController.cs
@@ -0,0 +1,27 @@
+﻿using JDOPSTest.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JDOPSTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StocksController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Stock> Get()
+        {
+            List<Stock> stocks = new List<Stock>();
+            for (int i = 0; i < 100; i++)
+            {
+                stocks.Add(new Stock
+                {
+                    ID = i,
+                    Name = "Stock " + i,
+                    Price = new Random().Next(20,200)
+                });
+            }
+            return stocks;
+        }
+    }
+}
---
+++ JDOPSTest/Models/Stock.cs
@@ -0,0 +1,9 @@
+﻿namespace JDOPSTest.Models
+{
+    public class Stock
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
